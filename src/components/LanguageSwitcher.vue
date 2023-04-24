0bd7316e3a89d8eec1780d95552c5d9154ef6259
Shivani K <shivanik.bbi18@rvce.edu.in>
2023-04-24 16:55:19 +0530

    modifications

--- src/components/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher.vue
@@ -1,5 +1,6 @@
 <template>
-    <label for="countries" class="block mb-1 text-sm py-5 px-4 text-gray-900 dark:text-white">Select an option</label>
+    <label 
+    class="block mb-1 text-sm py-5 px-4 text-gray-900 dark:text-white">Select a locale</label>
     <select @change="switchLanguage" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 m-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         <option v-for="sLocale in supportedLocales" :key="`locale-${sLocale}`" :value="sLocale"
             :selected="locale === sLocale">
