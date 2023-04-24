3dab6bf94d28dc88e5a900fbcf8827e7dc7b8495
Shivani K <shivanik.bbi18@rvce.edu.in>
2023-04-24 14:56:42 +0530

    vue app with internationalization

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -5,39 +5,6 @@ import datetimeFormats from "./rules/datetime";
 import en from "./locales/en.json";
 import ru from "./locales/ru.json" ;
 
-const messages = {
-    en: {
-        nav: {
-            home: "Home",
-            about: "About"
-        },
-        home: {
-            header: "Welcome to the Vue 3 I18n tutorial!",
-            created_by: "This tutorial was brought to you by {company}.",
-            num_visits: "This page hasn't been visited :( | This page has been visited {n} time | This page has been visited {n} times"
-        },
-        about: {
-            header: "About us",
-            donations: "Donations raised: {donations}"
-        }
-    },
-    ru: {
-        nav: {
-            home: "Главная",
-            about: "O нас"
-        },
-        home: {
-            header: "Добро пожаловать в руководство Vue 3 I18n!",
-            created_by: "Это руководство создано для вас компанией {company}.",
-            num_visits: "Страницу не посещали :( | Страницу посетили {n} раз | Страницу посетили {n} раза | Страницу посетили {n} раз"
-        },
-        about: {
-            header: "O нас",
-            donations: "Пожертвований собрано: {donations}"
-        }
-    }
-}
-
 export default createI18n({
     locale: import.meta.env.VITE_DEFAULT_LOCALE,
     fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
